9960cec1fd034b58e1ee59b01c7872d2bcba52fb
qw893821 <xiuman893821@gmail.com>
2018-08-26 17:02:38 -0400

    update
    try render texture to make it possible

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos;
+        pos = transform.position;
+        pos.x = player.transform.position.x;
+        transform.position = pos;
+	}
+}
---
+++ Assets/LookTowardCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardCamera : MonoBehaviour {
+    public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
